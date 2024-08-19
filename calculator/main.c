fbadbee74d7db8932a7c92b2a1e4528d9e7645fb
Chu-rill <churchillrimamskep@gmail.com>
2024-08-19 15:43:09 +0100

    a calculator

---
+++ calculator/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+   
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    printf("\n Enter an operator (+,-,*,/): ");
+    scanf("%c", &operator);
+
+    printf("\n Enter first number")
+    scanf("%lf", & num1)
+
+     printf("\n Enter second number")
+    scanf("%lf", & num2)
+
+   return 0
+}
