2e53aaa3c51f42e9a1424e662c3ed1cddef7da21
Chu-rill <churchillrimamskep@gmail.com>
2024-08-24 21:14:05 +0100

    made a clock in c

---
+++ clock/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <unistd.h>  // For usleep function
+#include <stdlib.h>  // For exit function
+
+
+int main(){
+    int h,m,s;
+    int d = 1000; // we add a delay of 10000 milliseconds
+    printf("Set time : \n");
+    scanf("%d%d%d",&h,&m,&s);
+    if(h > 12 || m > 60 || s > 60){
+        printf("Error ! \n");
+        exit(0);
+    }
+    while(1){
+        s++;
+        if(s > 59){
+            m++;
+            s=0;
+        }
+        if(m > 59){
+            h++;
+            m = 0;
+        }
+        if(h > 12){
+            h=1;
+        }
+       
+         printf("\n %02d:%02d:%02d", h, m, s);
+         printf("\n Clock : ");
+         usleep(d * 1000);  // Sleep for d milliseconds
+        system("clear");   // Clear the screen
+    }
+    return 0;
+}
